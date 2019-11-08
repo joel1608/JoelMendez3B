644ca135073bff9724e05a6410b534d71d7ea024
LUIS JOEL MENDEZ LOOR <e1312386921@live.uleam.edu.ec>
2019-11-08 18:49:06 -0500

    Demo

---
+++ FacturaGasolina/FacturaGasolina/clase/Cliente.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacturaGasolina.clase
+{
+    class Cliente
+    {
+       
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private int identidicacion;
+
+        public int Identificacion
+        {
+            get { return identidicacion; }
+            set { identidicacion = value; }
+        }
+
+       
+        private string direccion;
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        private List<Gasolina> gasolina;
+
+        public List<Gasolina> Gasolina
+        {
+            get { return gasolina; }
+            set { gasolina = value; }            
+        }
+
+        
+
+        public Cliente(string nombre, int identificacion, string direccion, List<Gasolina> gasolina)
+        {
+            
+            Nombre = nombre;
+            Identificacion = identificacion;
+            Direccion = direccion;
+            Gasolina = gasolina;
+           
+        }
+    }
+}
---
+++ FacturaGasolina/FacturaGasolina/clase/Gasolina.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacturaGasolina.clase
+{
+    class Gasolina
+    {
+        private string tipo;
+
+        public string Tipo
+        {
+            get { return tipo; }
+            set {tipo = value;}
+
+        }
+
+        private int galones;
+
+        public int Galones
+        {
+            get { return galones; }
+            set { galones = value; }
+        }
+
+        private float precio;
+
+        public float Precio
+        {
+            get
+            {
+                if (Tipo=="Extra")
+                    {
+                        precio=1.5f;
+                    }
+                    if (Tipo=="Super")
+                    {
+                        precio = 2;
+                    }
+               
+                    return precio;
+             }
+           
+        }
+         
+        public float Subtotal ()
+        {
+            return Precio * Galones;
+        }
+
+       public float Iva ()
+        {
+            return Subtotal() * 0.12f;
+        }
+
+        public float Total()
+        {
+            return Iva() + Subtotal();
+        }
+
+        private Cliente cliente;
+
+        public Cliente Cliente
+        {
+            get { return cliente; }
+            set { cliente = value; }
+        }
+
+
+        public Gasolina(string tipo, int galones)
+        {
+            Tipo = tipo;
+            Galones = galones;
+        
+        }
+    }
+}
